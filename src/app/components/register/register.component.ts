9bdfdcda03751871a1c90902ecda6e3bc19001e3
Abhay078 <abhaysrivastava078@gmail.com>
2023-05-05 19:05:46 +0530

    [Abhay]-register component added with validation and sending data to api

--- src/app/components/register/register.component.ts
+++ src/app/components/register/register.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup,Validators } from '@angular/forms';
+import { UserService } from 'src/services/UserService/user.service';
 
 @Component({
   selector: 'app-register',
@@ -11,26 +12,37 @@ export class RegisterComponent implements OnInit {
   submitted=false;
   showPass=false;
 
-  constructor(private formBuilder:FormBuilder) { }
+  constructor(private formBuilder:FormBuilder,private user:UserService) { }
 
   ngOnInit(){
     this.registerForm=this.formBuilder.group({
-      firstName:['',Validators.required],
-      lastName:['',Validators.required],
+      firstName:['',[Validators.required,Validators.pattern(/[A-Z]{1}[a-z]{1,}/)]],
+      lastName:['',[Validators.required,Validators.pattern(/[A-Z]{1}[a-z]{1,}/)]],
       email:['',[Validators.required,Validators.email]],
-      password:['',[Validators.required,Validators.minLength(8)]],
+      password:['',[Validators.required,Validators.minLength(8),Validators.pattern(/^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*\W)(?!.* ).{8}/)]],
       confirmPassword:['',[Validators.required]]
 
     });
   }
-  // get f(){return this.registerForm.controls;}
 
   onSubmit(){
     this.submitted=true;
     if(this.registerForm.invalid){
+      console.log("Enter Valid data");
       return;
     }
-    alert('Success!!');
+    else{
+      let regData={
+        "firstName":this.registerForm.value.firstName,
+        "lastName":this.registerForm.value.lastName ,
+        "email":this.registerForm.value.email ,
+        "password":this.registerForm.value.password
+      }
+      this.user.registration(regData).subscribe((response:any)=>{
+        console.log(response);
+      })
+      
+    }
   }
   
 
