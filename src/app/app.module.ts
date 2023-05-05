b2925c9da5087e2f15d83b6d25690d59838b4e33
Abhay078 <abhaysrivastava078@gmail.com>
2023-05-05 11:24:40 +0530

    [Abhay]-app module updated

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { ReactiveFormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { RegisterComponent } from './components/register/register.component';
@@ -11,6 +11,7 @@ import {MatInputModule} from '@angular/material/input';
 import {MatButtonModule} from '@angular/material/button';
 import {MatCheckboxModule} from '@angular/material/checkbox';
 import { ForgetComponent } from './components/forget/forget.component';
+import { ResetPassComponent } from './components/reset-pass/reset-pass.component';
 
 
 @NgModule({
@@ -18,10 +19,12 @@ import { ForgetComponent } from './components/forget/forget.component';
     AppComponent,
     RegisterComponent,
     LoginComponent,
-    ForgetComponent
+    ForgetComponent,
+    ResetPassComponent
   ],
   imports: [
     BrowserModule,
+    ReactiveFormsModule,
     AppRoutingModule,
     BrowserAnimationsModule,
     MatFormFieldModule,
