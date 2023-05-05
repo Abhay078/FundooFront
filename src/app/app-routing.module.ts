72a0f820c6363b555c641ffbc9b9d7633f57bc54
Abhay078 <abhaysrivastava078@gmail.com>
2023-05-05 11:23:59 +0530

    [Abhay]-routing added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { RegisterComponent } from './components/register/register.component';
+import { ForgetComponent } from './components/forget/forget.component';
+import { ResetPassComponent } from './components/reset-pass/reset-pass.component';
+import { LoginComponent } from './components/login/login.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'',redirectTo:'/login',pathMatch:'full'},
+  {path:'login',component:LoginComponent},
+  {path:'register',component:RegisterComponent},
+  {path:'forget',component:ForgetComponent},
+  {path:'reset',component:ResetPassComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
