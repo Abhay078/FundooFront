813cf21c5d4855b9c02e4566b137ed69435df7f0
Abhay078 <abhaysrivastava078@gmail.com>
2023-05-09 20:16:19 +0530

    [Abhay]-note service created and getnote and addnote points created

---
+++ src/services/NoteService/note-service.service.ts
@@ -0,0 +1,33 @@
+import { Injectable } from '@angular/core';
+import { HttpService } from '../HttpService/http.service';
+import { HttpHeaders } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class NoteServiceService {
+ tokenValue:any;
+  constructor(private http:HttpService) { 
+    this.tokenValue=localStorage.getItem('token')
+  }
+
+  AddNote(data:any){
+    let header={
+      headers:new HttpHeaders({
+      'Content-Type':'application/json',
+      'Authorization':'Bearer '+this.tokenValue
+    })
+  }
+   return this.http.postMethod('Notes',data,true,header)
+  }
+
+  GetNote(){
+    let header={
+      headers:new HttpHeaders({
+      'Content-Type':'application/json',
+      'Authorization':'Bearer '+this.tokenValue
+    })
+  }
+  return this.http.getMethod('Notes',true,header)
+}
+}
