98f98a43dfccc7faaded11db5330c7919a53d527
Abhay078 <abhaysrivastava078@gmail.com>
2023-05-09 12:18:15 +0530

    [Abhay]-Httpservice resetpassword service added

--- src/services/HttpService/http.service.ts
+++ src/services/HttpService/http.service.ts
@@ -1,15 +1,16 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class HttpService {
-
+  baseUrl: string = environment.baseUrl
   constructor(private http: HttpClient) { }
 
   postMethod(url: string, payload: {}, token: boolean, httpOption: any) {
-    return this.http.post(url, payload, token && httpOption)
+    return this.http.post(this.baseUrl+url, payload, token && httpOption)
   }
   
 }
