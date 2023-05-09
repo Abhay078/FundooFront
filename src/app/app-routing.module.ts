b9612658928adcca2aedff68fd75b815749a32c4
Abhay078 <abhaysrivastava078@gmail.com>
2023-05-09 10:09:04 +0530

    [Abhay]-routing for dashboard,addnote,display and icons added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,13 +4,23 @@ import { RegisterComponent } from './components/register/register.component';
 import { ForgetComponent } from './components/forget/forget.component';
 import { ResetPassComponent } from './components/reset-pass/reset-pass.component';
 import { LoginComponent } from './components/login/login.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { AddNoteComponent } from './components/add-note/add-note.component';
+import { DisplayNoteComponent } from './components/display-note/display-note.component';
+import { IconsComponent } from './components/icons/icons.component';
 
 const routes: Routes = [
   {path:'',redirectTo:'/login',pathMatch:'full'},
   {path:'login',component:LoginComponent},
   {path:'register',component:RegisterComponent},
   {path:'forget',component:ForgetComponent},
-  {path:'resetPassword',component:ResetPassComponent}
+  {path:'resetPassword',component:ResetPassComponent},
+  {path:'home',component:DashboardComponent},
+  {path:'addNote',component:AddNoteComponent},
+  {path:'displayNote',component:DisplayNoteComponent},
+  {path:'icon',component:IconsComponent},
+
+
 ];
 
 @NgModule({
