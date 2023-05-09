749d3befb01898b16e3bd0008dde245240388cda
Abhay078 <abhaysrivastava078@gmail.com>
2023-05-09 20:39:37 +0530

    [Abhay]-notes component added with child component

---
+++ src/app/components/notes/notes.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+import { NoteServiceService } from 'src/services/NoteService/note-service.service';
+
+@Component({
+  selector: 'app-notes',
+  templateUrl: './notes.component.html',
+  styleUrls: ['./notes.component.scss']
+})
+export class NotesComponent implements OnInit {
+
+  constructor(private note:NoteServiceService) { }
+
+  ngOnInit(): void {
+    this.getNote();
+  }
+  getNote(){
+    this.note.GetNote().subscribe((res:any)=>{
+      console.log(res);
+    })
+  }
+}
