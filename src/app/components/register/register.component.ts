d71bf2a60a7451faf99b73267dadde281191025a
Abhay078 <abhaysrivastava078@gmail.com>
2023-05-19 10:55:33 +0530

    [Abhay]-snackbar added to register page

--- src/app/components/register/register.component.ts
+++ src/app/components/register/register.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup,Validators } from '@angular/forms';
+import { MatSnackBar } from '@angular/material/snack-bar';
 import { UserService } from 'src/services/UserService/user.service';
 
 @Component({
@@ -12,7 +13,7 @@ export class RegisterComponent implements OnInit {
   submitted=false;
   showPass=false;
 
-  constructor(private formBuilder:FormBuilder,private user:UserService) { }
+  constructor(private formBuilder:FormBuilder,private user:UserService,private _snackBar: MatSnackBar) { }
 
   ngOnInit(){
     this.registerForm=this.formBuilder.group({
@@ -29,6 +30,7 @@ export class RegisterComponent implements OnInit {
     this.submitted=true;
     if(this.registerForm.invalid){
       console.log("Enter Valid data");
+      this._snackBar.open('Enter Valid Data','Close');
       return;
     }
     else{
@@ -40,6 +42,11 @@ export class RegisterComponent implements OnInit {
       }
       this.user.registration(regData).subscribe((response:any)=>{
         console.log(response);
+        this._snackBar.open('Registration Successfull','Close');
+      },(error)=>{
+        console.log(error)
+        console.log(error.status);
+        this._snackBar.open('Registration Unsuccessfull','Close');
       })
       
     }
