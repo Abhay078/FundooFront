3eb7a39b3e901d656be918bcacbf2325d8bde5e5
Abhay078 <abhaysrivastava078@gmail.com>
2023-05-19 10:57:38 +0530

    [Abhay]-changes in some code

--- src/app/components/add-note/add-note.component.ts
+++ src/app/components/add-note/add-note.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 import { NoteServiceService } from 'src/services/NoteService/note-service.service';
 
 @Component({
