c480cc6d7cc06abddc7de1c069c9cef140edea92
Abhay078 <abhaysrivastava078@gmail.com>
2023-05-30 10:52:37 +0530

    [Abhay]-added imports for testing

--- src/app/authentication.guard.ts
+++ src/app/authentication.guard.ts
@@ -1,6 +1,5 @@
 import { Injectable } from '@angular/core';
 import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree } from '@angular/router';
-import { Observable } from 'rxjs';
 import { AuthguardServiceService } from 'src/services/AuthService/authguard-service.service';
 
 @Injectable({
