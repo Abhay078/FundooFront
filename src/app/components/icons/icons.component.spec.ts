80e2db3b1b85c16440b36b16e705b2d453317bac
Abhay078 <abhaysrivastava078@gmail.com>
2023-05-30 10:45:58 +0530

    [Abhay]-added imports in spec.ts

--- src/app/components/icons/icons.component.spec.ts
+++ src/app/components/icons/icons.component.spec.ts
@@ -1,6 +1,11 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { NoteServiceService } from 'src/services/NoteService/note-service.service';
 import { IconsComponent } from './icons.component';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { ActivatedRoute } from '@angular/router';
+import { RouterTestingModule } from '@angular/router/testing';
+import { MatMenuModule } from '@angular/material/menu';
 
 describe('IconsComponent', () => {
   let component: IconsComponent;
@@ -8,7 +13,11 @@ describe('IconsComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ IconsComponent ]
+      declarations: [ IconsComponent ],
+      imports:[
+        HttpClientTestingModule,MatSnackBarModule,
+        RouterTestingModule,MatMenuModule
+      ]
     })
     .compileComponents();
   });
