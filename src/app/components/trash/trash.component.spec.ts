dff7a4ca51e2bc060b716ad4c4337aa6964d613d
Abhay078 <abhaysrivastava078@gmail.com>
2023-05-30 10:47:59 +0530

    [Abhay]-added imports in spec.ts

--- src/app/components/trash/trash.component.spec.ts
+++ src/app/components/trash/trash.component.spec.ts
@@ -1,6 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { TrashComponent } from './trash.component';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 
 describe('TrashComponent', () => {
   let component: TrashComponent;
@@ -8,7 +9,10 @@ describe('TrashComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ TrashComponent ]
+      declarations: [ TrashComponent ],
+      imports:[
+        HttpClientTestingModule
+      ]
     })
     .compileComponents();
   });
