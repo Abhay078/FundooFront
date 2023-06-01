78724967af4e0cf30e6d177d6c03e8cf900bfce2
Abhay078 <abhaysrivastava078@gmail.com>
2023-06-01 10:03:16 +0530

    [Abhay]-added test cases for login component

--- src/app/components/login/login.component.spec.ts
+++ src/app/components/login/login.component.spec.ts
@@ -1,4 +1,4 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, async } from '@angular/core/testing';
 
 import { LoginComponent } from './login.component';
 import { FormBuilder, ReactiveFormsModule } from '@angular/forms';
@@ -6,6 +6,7 @@ import { HttpClientModule } from '@angular/common/http';
 import { RouterModule } from '@angular/router';
 import { RouterTestingModule } from '@angular/router/testing';
 import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { By } from '@angular/platform-browser';
 
 describe('LoginComponent', () => {
   let component: LoginComponent;
@@ -31,4 +32,38 @@ describe('LoginComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+  it('should validate password',async(()=>{
+    let password=component.LoginForm.controls['password']
+    password.setValue('John')
+    fixture.detectChanges();
+    expect(password.valid).toBeFalsy();
+  }))
+
+  it('should validate email',async(()=>{
+    let email=component.LoginForm.controls['email']
+    email.setValue('John123')
+    fixture.detectChanges();
+    expect(email.valid).toBeFalsy();
+  }))
+
+
+  it('should route correctly to register page on clicking on create account',async(()=>{
+    const div=fixture.nativeElement.querySelector('.b-link');
+    const anchor=div.querySelector('a');
+    expect(anchor.getAttribute('routerLink')).toBe('/register')
+  }))
+
+  it('should route correctly to forget page on clicking on forget password',async(()=>{
+    const el=fixture.debugElement.nativeElement;
+    const anchor=el.querySelector('a')
+    expect(anchor.getAttribute('routerLink')).toBe('/forget')
+  }))
+
+  it('should call onSubmit method',async(()=>{
+    fixture.detectChanges();
+    spyOn(component,'onSubmit')
+    const el=fixture.debugElement.query(By.css('button')).nativeElement;
+    el.click();
+    expect(component.onSubmit).toHaveBeenCalledTimes(1);
+  }))
 });
