fd60f9f1cacbf0f98d2fa333f1cab33a905cb0dd
Abhay078 <abhaysrivastava078@gmail.com>
2023-06-01 10:05:32 +0530

    [Abhay]-added imports for test cases

--- src/services/NoteService/note-service.service.spec.ts
+++ src/services/NoteService/note-service.service.spec.ts
@@ -1,12 +1,17 @@
 import { TestBed } from '@angular/core/testing';
 
 import { NoteServiceService } from './note-service.service';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 
 describe('NoteServiceService', () => {
   let service: NoteServiceService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports:[
+        HttpClientTestingModule
+      ]
+    });
     service = TestBed.inject(NoteServiceService);
   });
 
